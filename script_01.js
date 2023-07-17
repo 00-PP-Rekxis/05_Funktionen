d07bb31bcf515f214b2c7564300e77b4e9f9e24a
Rekxis <Rekxis@gmx.de>
2023-07-17 11:44:41 +0200

    2c. Mehrere Parameter / Argumente

--- script_01.js
+++ script_01.js
@@ -30,12 +30,21 @@ function ausgabeNamen() {
 // 2b. Parametrisierung + Datenübergabe von AUSSEN
 
 // Arugmente sind Daten für PArameter
-ausgabeNamenZwei("Peter"); // call + Argument(e)
-ausgabeNamenZwei("Sohrab");
-ausgabeNamenZwei("Yalcin");
+// ausgabeNamenZwei("Peter"); // call + Argument(e)
+// ausgabeNamenZwei("Sohrab");
+// ausgabeNamenZwei("Yalcin");
 
 // Funktion mit Parameter(n)
 function ausgabeNamenZwei(firstName) {
     console.log(`Hallo ${firstName}!`);
 }
 
+/***** Funktionen 02c *****/
+// 2c. Mehrere Parameter / Argumente
+
+ausgabeNamenParams("Max", "Muetze")
+ausgabeNamenParams(prompt("Vorname?"), prompt("Name?"))
+
+function ausgabeNamenParams(firstName, familyName) {
+    console.log(`Hallo ${firstName} ${familyName}!`);
+}
