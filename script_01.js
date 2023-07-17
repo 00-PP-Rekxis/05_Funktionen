c8653e612167f8c4a0abcafd551fc4533e5a7aca
Rekxis <Rekxis@gmx.de>
2023-07-17 10:21:11 +0200

    1. Kapselung von Codeblöcken

--- script_01.js
+++ script_01.js
@@ -3,3 +3,15 @@
 /***** Funktionen 01 *****/
 // 1. Kapselung von Codeblöcken
 
+// Funktionsaufruf (call)
+// test();
+
+// Funktionsrumpf (body) | callee
+// Funktionsdeklaration
+function test()
+{
+    console.log("Hey.");
+}
+
+
+
